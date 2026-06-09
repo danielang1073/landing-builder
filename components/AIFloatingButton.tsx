575e7cc1b8be133fe5b713f39c29ca101fa467b9
Daniel Cruz <dcruz@inszoneins.com>
2026-06-09 12:42:21 -0500

    text

--- components/AIFloatingButton.tsx
+++ components/AIFloatingButton.tsx
@@ -284,7 +284,7 @@ export default function AIFloatingButton() {
                   : "text-gray-500 hover:text-gray-700"
               }`}
             >
-              Desde imagen
+              From Image
             </button>
           </div>
 
